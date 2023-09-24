99a148c1fe6778859151fb4fb8be84d4a044bfcc
Pui Ching Chung <lawchunni@yahoo.com.hk>
2023-09-24 13:30:57 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './pages/Home';
 import './assets/styles/main.scss';
 import { useEffect } from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -9,12 +10,13 @@ function App() {
     document.title = "JENNY's Portfolio | Home";
   }, []);
   return (
-    <div>
+    <>
       <div className="App">
         <Header />
         <Home />
+        <Footer />
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import linkedin from '../assets/images/icon-linkedin.svg';
+import whatsapp from '../assets/images/icon-whatsapp.svg';
+
+function Footer() {
+
+  const date = new Date();
+  const getYear = date.getFullYear();
+
+  return (
+    <>
+      <footer>
+        <div className="wrapper">
+          <div className="left">
+            <p>Copyright &copy; { getYear } JennyPortfolio. All right reserved.</p>
+          </div>
+          <div className="right icon">
+            <div className="social_icon">
+              <a href="https://www.linkedin.com/in/jenny-puichingchung/" target="_blank" title="Linkedin">
+                <img src={ linkedin } alt="linkedin icon" width={35} height={35} />
+              </a>
+            </div>
+            <div className="social_icon">
+              <a href="https://api.whatsapp.com/send?phone=12049968808" target="_blank" title="Whatsapp">
+                <img src={ whatsapp } alt="whatsapp icon" width={35} height={35} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer;
