e571565493da4ed29c20c4639a263644d6e03799
Pui Ching Chung <lawchunni@yahoo.com.hk>
2024-03-05 22:22:35 -0600

    created data loading & error components

---
+++ src/components/common/Error.js
@@ -0,0 +1,11 @@
+function Error () {
+  return (
+    <>
+      <div className="message error">
+        Failed to load data.
+      </div>
+    </>
+  )
+}
+
+export default Error;
---
+++ src/components/common/Loading.js
@@ -0,0 +1,11 @@
+function Loading () {
+  return (
+    <>
+      <div className="message loading">
+        Loading...
+      </div>
+    </>
+  )
+}
+
+export default Loading;
