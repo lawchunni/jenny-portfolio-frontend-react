f9893d9155d11551d9f5f53302a00f930b7ee6f2
Pui Ching Chung <lawchunni@yahoo.com.hk>
2024-10-06 16:24:20 -0400

    [Login] Added form validation and sanitization to the form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,32 @@
 import { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { loginApi } from "../services/authApi";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import DOMPurify from "dompurify";
 
-function Login() {
+const Login = () => {
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const schema = yup.object().shape({
+    username: yup.string().required('Username is required'),
+    password: yup.string().required('Password is required')
+  })
+
   const [error, setError] = useState(null);
   const { login } = useAuth();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // apply form validation and handle submit
+  const { register, handleSubmit, formState: { errors }} = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = async (data) => {
+
+    const username = DOMPurify.sanitize(data.username);
+    const password = DOMPurify.sanitize(data.password);
 
     const verifyUserOnServer = async () => {
-
       try {
         const data = await loginApi(username, password);
         await login(data);
@@ -24,9 +37,6 @@ function Login() {
     }
 
     verifyUserOnServer();
-
-    setUsername('');
-    setPassword('');
   };
 
   return (
@@ -35,21 +45,29 @@ function Login() {
         <div className="wrapper">
           <div className="content">
             <h1>Login</h1>
-            <form onSubmit={handleSubmit} noValidate>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
               <p className="field required">
-                <input type="text" name="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required/>
-                {/* Add escape to 'value' */}
-                  {/* <span className="error"><?=$utils->esc($errors['email'][0] ?? '') ?></span> */}
+                <input 
+                  type="text" 
+                  name="username" 
+                  placeholder="Username" 
+                  {...register('username')} />
+
+                {errors.username && <span className="error">{errors.username?.message}</span>}
               </p>
               <p className="field required">
-                <input type="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-                {/* <span className="error"><?=$utils->esc($errors['password'][0] ?? '') ?></span> */}
+                <input 
+                  type="password" 
+                  name="password" 
+                  placeholder="Password" 
+                  {...register('password')} />
+
+                {errors.password && <span className="error">{errors.password?.message}</span>}
               </p>
               <p>
                 <button className="submit_button">Submit</button>
               </p>
             </form>
-            {/* <div className="forget_password"><a href="#">Forget Password?</a></div> */}
           </div>
       </div>
     </section>
