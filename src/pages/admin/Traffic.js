fcc994974b875d0c4a075d22a2d96464e7d77062
Pui Ching Chung <lawchunni@yahoo.com.hk>
2024-10-06 15:34:57 -0400

    [Traffic] Added dummy page

---
+++ src/pages/admin/Traffic.js
@@ -0,0 +1,23 @@
+const Traffic = () => {
+  return (
+    <>
+      <section id="admin_traffic">
+        <div className="wrapper">
+          <div className="content">
+
+            <h1>Traffic</h1>
+
+
+
+            <div className="view_list" style={{ textAlign: 'center'}}>
+              Under Construction...
+            </div>
+            
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Traffic;
